94be14fa5a8406a5797d80715cc4b7ba9be0a994
MK884 <marchantkhalid7810@gmail.com>
2024-10-14 00:45:46 +0530

    test

--- client/app/(app)/(screen)/storePage.tsx
+++ client/app/(app)/(screen)/storePage.tsx
@@ -21,6 +21,7 @@ import Animated, {
   useAnimatedScrollHandler,
   useAnimatedStyle,
   useSharedValue,
+  withTiming,
 } from 'react-native-reanimated';
 
 const storePage = () => {
@@ -182,17 +183,11 @@ const storePage = () => {
       Extrapolation.CLAMP
     );
 
-    const scale = interpolate(offsetY.value, [0, LOGO_SIZE], [1, 0.4], {
-      extrapolateRight: Extrapolation.CLAMP,
-    });
     return {
       transform: [
         {
           translateY,
         },
-        // {
-        //   scale,
-        // },
         {
           translateX,
         },
@@ -202,24 +197,25 @@ const storePage = () => {
 
   const sectionStyle = useAnimatedStyle(() => {
     const opacity = interpolate(offsetY.value, [0, LOGO_SIZE], [1, 0]);
-    // const display = interpolate(offsetY.value, [0, LOGO_SIZE], []);
-    // const height = interpolate(offsetY.value, [0, LOGO_SIZE],[0])
+
     const translateY = interpolate(
       offsetY.value,
-      [0, LOGO_SIZE * 2 + HEADER_HEIGHT * 2],
+      [0, offsetY.value],
       [0, -offsetY.value]
     );
 
     const height = interpolate(
       offsetY.value,
-      [0, LOGO_SIZE * 2 + HEADER_HEIGHT],
-      [LOGO_SIZE * 2 + HEADER_HEIGHT, 0]
+      [0, LOGO_SIZE * 2],
+      [LOGO_SIZE * 2 + HEADER_HEIGHT, HEADER_HEIGHT],
+      Extrapolation.CLAMP
     );
 
     return {
-      transform: [{ translateY: -offsetY.value }],
+      transform: [{ translateY }],
       // top: -offsetY.value,
       // opacity,
+      // height,
     };
   });
 
@@ -227,7 +223,7 @@ const storePage = () => {
     const flex = interpolate(offsetY.value, [0, LOGO_SIZE], [0, 1]);
     const translateY = interpolate(
       offsetY.value,
-      [0, LOGO_SIZE],
+      [0, offsetY.value],
       [0, -offsetY.value],
       Extrapolation.CLAMP
     );
@@ -239,90 +235,65 @@ const storePage = () => {
   return (
     <SafeAreaView className="bg-white pt-4" style={{ height, width }}>
       <View
-        className="bg-transparent"
-        style={{ paddingHorizontal, height: 25 }}
+        className="bg-transparent justify-center"
+        style={{ paddingHorizontal, height: 40 }}
       >
         <Pressable onPress={() => router.back()}>
           <Feather name="arrow-left" size={28} color="black" />
         </Pressable>
       </View>
+
       {/*hero section */}
-      <Animated.View
-        className="items-center space-y-2 pt-28"
-        style={[sectionStyle]}
-      >
-        <Animated.View
-          className="rounded-full overflow-hidden border-4 border-[#dadada] bg-white"
-          // style={[
-          //   {
-          //     height: LOGO_SIZE,
-          //     aspectRatio: 1,
-          //     transform: [
-          //       { translateY: -(LOGO_SIZE + HEADER_HEIGHT - 8) },
-          //       { scale: 0.4 },
-          //       { translateX: -300 },
-          //     ],
-          //   },
-          // ]}
-          style={[{ height: LOGO_SIZE, aspectRatio: 1 }, animatedImageStyle]}
-        >
-          <Animated.Image
-            source={{ uri: store?.avatarUrl }}
-            style={[{ resizeMode: 'contain', height: '100%', width: '100%' }]}
-          />
-        </Animated.View>
-        <Animated.View
-          // style={{
-          //   transform: [
-          //     { translateY: -(LOGO_SIZE + HEADER_HEIGHT * 2 + 10) },
-          //     { translateX: -LOGO_SIZE / 2 },
-          //     { scale: 0.6 },
-          //   ],
-          // }}
-          style={animatedTextStyle}
-        >
-          <MyText className="text-black font-extrabold text-[32px] capitalize">
-            {store?.name}
-          </MyText>
-        </Animated.View>
-        <View className="flex-row gap-2">
-          <MaterialIcons name="verified" size={24} color="green" />
-          <MyText className="text-[#868687] text-[16px]">
-            Verified official store
-          </MyText>
-        </View>
-      </Animated.View>
-      {/* tags */}
-      <Animated.View
-        // style={[
-        //   {
-        //     marginLeft: paddingHorizontal,
-        //     transform: [
-        //       { translateY: -(LOGO_SIZE / 2 + HEADER_HEIGHT * 4 + 12) },
-        //       { translateX: LOGO_SIZE },
-        //     ],
-        //   },
-        // ]}
-        style={animatedTagStyle}
-        className="flex flex-row my-2 items-center justify-center"
-      >
-        {tabs?.map((item) => (
-          <CategoryLabel
-            isSelected={item?.label === selectedCategory}
-            label={item.label}
-            onPress={() => setSelectedCategory(item.label)}
-            key={item.label}
-          />
-        ))}
-      </Animated.View>
+
       <Animated.ScrollView
         showsVerticalScrollIndicator={false}
         onScroll={handleScroll}
         bounces={false}
+        scrollEventThrottle={16}
+        className="flex-1"
       >
-        <View>
+        <Animated.View
+          className="items-center space-y-2 pt-28"
+          style={[sectionStyle]}
+        >
+          <Animated.View
+            className="rounded-full overflow-hidden border-4 border-[#dadada] bg-white"
+            style={[{ height: LOGO_SIZE, aspectRatio: 1 }, animatedImageStyle]}
+          >
+            <Animated.Image
+              source={{ uri: store?.avatarUrl }}
+              style={[{ resizeMode: 'contain', height: '100%', width: '100%' }]}
+            />
+          </Animated.View>
+          <Animated.View style={animatedTextStyle}>
+            <MyText className="text-black font-extrabold text-[32px] capitalize">
+              {store?.name}
+            </MyText>
+          </Animated.View>
+          <View className="flex-row gap-2">
+            <MaterialIcons name="verified" size={24} color="green" />
+            <MyText className="text-[#868687] text-[16px]">
+              Verified official store
+            </MyText>
+          </View>
+        </Animated.View>
+        {/* tags */}
+        <Animated.View
+          style={animatedTagStyle}
+          className="flex flex-row my-2 items-center justify-center "
+        >
+          {tabs?.map((item) => (
+            <CategoryLabel
+              isSelected={item?.label === selectedCategory}
+              label={item.label}
+              onPress={() => setSelectedCategory(item.label)}
+              key={item.label}
+            />
+          ))}
+        </Animated.View>
+        <Animated.View className="">
           {tabs.map((tab) => tab.label === selectedCategory && tab.content)}
-        </View>
+        </Animated.View>
       </Animated.ScrollView>
     </SafeAreaView>
   );
--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,38 +1,239 @@
-import { OnBoardingScreen } from '@/components';
-import { onBoardData } from '@/constants';
-import {
-  Montserrat_100Thin,
-  Montserrat_400Regular,
-  useFonts,
-} from '@expo-google-fonts/montserrat';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import { View } from 'react-native';
+import Feather from '@expo/vector-icons/Feather';
+import React from 'react';
+import { Dimensions, Pressable, Text, View } from 'react-native';
+import Animated, {
+  Extrapolation,
+  interpolate,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { CategoryLabel } from '@/components';
 
-export default function App() {
-  const [loaded, error] = useFonts({
-    Montserrat_100Thin,
-    Montserrat_400Regular,
+const tabs: tab[] = [
+  {
+    label: 'overview',
+    content: (
+      <View className="">
+        <Text>Overview</Text>
+      </View>
+    ),
+  },
+  {
+    label: 'about',
+    content: (
+      <View>
+        <Text>About</Text>
+      </View>
+    ),
+  },
+];
+
+const index = () => {
+  const LOGO_SIZE = 120;
+  const HEADER_HEIGHT = 60;
+  const paddingHorizontal = 22;
+  const constantTPadding = 80;
+
+  const { height, width } = Dimensions.get('screen');
+  const [selectedCategory, setSelectedCategory] = React.useState<
+    'overview' | 'about' | string
+  >('overview');
+
+  const scrollY = useSharedValue(0);
+
+  const hadnleScroll = useAnimatedScrollHandler((e) => {
+    scrollY.value = e.contentOffset.y;
+    console.log('y =>', e.contentOffset.y);
   });
 
-  useEffect(() => {
-    if (loaded || error) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded, error]);
+  // image style
+  const ImageStyle = useAnimatedStyle(() => {
+    const translateY = interpolate(
+      scrollY.value,
+      [0, HEADER_HEIGHT + constantTPadding],
+      [0, scrollY.value - (HEADER_HEIGHT + LOGO_SIZE / 4) - constantTPadding],
+      Extrapolation.CLAMP
+    );
 
-  if (!loaded && !error) {
-    return null;
-  }
+    const translateX = interpolate(
+      scrollY.value,
+      [0, HEADER_HEIGHT + constantTPadding],
+      [0, -HEADER_HEIGHT * 2],
+      Extrapolation.CLAMP
+    );
+
+    const scale = interpolate(
+      scrollY.value,
+      [0, HEADER_HEIGHT + constantTPadding],
+      [1, 0.4],
+      Extrapolation.CLAMP
+    );
+
+    return {
+      transform: [{ translateY }, { translateX }, { scale }],
+    };
+  });
+
+  // header style
+  const headerStyle = useAnimatedStyle(() => {
+    return { transform: [{ translateY: scrollY.value }] };
+  });
+
+  // text style
+  const textStyle = useAnimatedStyle(() => {
+    const translateY = interpolate(
+      scrollY.value,
+      [0, HEADER_HEIGHT + constantTPadding + 10],
+      [0, scrollY.value - (HEADER_HEIGHT * 2 + LOGO_SIZE + 10)],
+      Extrapolation.CLAMP
+    );
+
+    const translateX = interpolate(
+      scrollY.value,
+      [0, HEADER_HEIGHT + constantTPadding + 10],
+      [0, -(HEADER_HEIGHT / 2 + LOGO_SIZE / 4)],
+      Extrapolation.CLAMP
+    );
+
+    const scale = interpolate(
+      scrollY.value,
+      [0, HEADER_HEIGHT + constantTPadding + 10],
+      [1, 0.6],
+      Extrapolation.CLAMP
+    );
+
+    return {
+      transform: [{ translateY }, { translateX }, { scale }],
+    };
+  });
+
+  // tag style
+  const animatedTagStyle = useAnimatedStyle(() => {
+    const translateY = interpolate(
+      scrollY.value,
+      [0, HEADER_HEIGHT + constantTPadding + 14],
+      [0, scrollY.value - (HEADER_HEIGHT + LOGO_SIZE * 2 + 14)],
+      Extrapolation.CLAMP
+    );
+
+    const translateX = interpolate(
+      scrollY.value,
+      [0, HEADER_HEIGHT + constantTPadding + 14],
+      [0, HEADER_HEIGHT * 2],
+      Extrapolation.CLAMP
+    );
+    return {
+      transform: [{ translateY }, { translateX }],
+    };
+  });
+
+  const textTagStyle = useAnimatedStyle(() => {
+    const opacity = interpolate(
+      scrollY.value,
+      [0, HEADER_HEIGHT + constantTPadding + 10],
+      [1, 0]
+    );
+
+    return {
+      opacity,
+    };
+  });
 
   return (
-    <SafeAreaView className="flex-1">
-      <View className="flex-1 bg-white">
-        <OnBoardingScreen items={onBoardData} />
-      </View>
-      <StatusBar style="auto" />
+    <SafeAreaView style={{ flex: 1 }}>
+      {/* header */}
+
+      <Animated.ScrollView
+        showsVerticalScrollIndicator={false}
+        className=""
+        onScroll={hadnleScroll}
+        scrollEventThrottle={16}
+        bounces={false}
+      >
+        {/* header */}
+        <Animated.View
+          className="justify-center "
+          style={[{ paddingHorizontal, height: HEADER_HEIGHT }, headerStyle]}
+        >
+          <Pressable>
+            <Feather name="arrow-left" size={28} color="black" />
+          </Pressable>
+        </Animated.View>
+        <View style={{ paddingTop: constantTPadding }} />
+        <Animated.View
+          className="rounded-full border-4 border-[#dadada] mx-auto"
+          style={[
+            {
+              height: LOGO_SIZE,
+              aspectRatio: 1,
+              // transform: [
+              //   { translateY: -(HEADER_HEIGHT + LOGO_SIZE / 4) },
+              //   { translateX: -HEADER_HEIGHT * 2 },
+              //   { scale: 0.4 },
+              // ],
+            },
+            ImageStyle,
+          ]}
+        />
+        <Animated.View
+          className="items-center"
+          style={[
+            {
+              //    transform: [
+              //   { translateY: -(HEADER_HEIGHT + LOGO_SIZE ) - (constantTPadding - 10)},
+              //   { translateX: -(HEADER_HEIGHT/2  + LOGO_SIZE / 4) },
+              //   { scale: 0.6 },
+              // ],
+            },
+            textStyle,
+          ]}
+        >
+          <Text className="text-black font-extrabold text-[32px] capitalize">
+            zara
+          </Text>
+        </Animated.View>
+        <Animated.View
+          className="flex-row gap-2 justify-center"
+          style={[textTagStyle]}
+        >
+          <MaterialIcons name="verified" size={24} color="green" />
+          <Text className="text-[#868687] text-[16px]">
+            Verified official store
+          </Text>
+        </Animated.View>
+        {/* tags */}
+        <Animated.View
+          // style={animatedTagStyle}
+          className="flex-row my-2 justify-center "
+          style={[
+            {
+              //   transform: [
+              //   { translateY: -(HEADER_HEIGHT + LOGO_SIZE * 2  + 14)},
+              //   { translateX: (HEADER_HEIGHT*2 ) },
+              // ],
+            },
+            animatedTagStyle,
+          ]}
+        >
+          {tabs?.map((item) => (
+            <CategoryLabel
+              isSelected={item?.label === selectedCategory}
+              label={item.label}
+              onPress={() => setSelectedCategory(item.label)}
+              key={item.label}
+            />
+          ))}
+        </Animated.View>
+        <View className="">
+          {tabs.map((tab) => tab.label === selectedCategory && tab.content)}
+        </View>
+        <View style={{ height: height * 2 }} />
+      </Animated.ScrollView>
     </SafeAreaView>
   );
-}
+};
+
+export default index;
